021523139ee24a4209987c2d4771dde7d2849fe1
shubhsonic <shubbham840@gmail.com>
2025-09-19 20:30:10 +0530

    added links on header

--- components/header.tsx
+++ components/header.tsx
@@ -53,7 +53,6 @@ export function Header() {
       <div className="container flex h-16 items-center justify-between">
         <div className="flex items-center">
           <a className="flex pl-4 items-center space-x-2 mr-8" href="/">
-            <div className="h-8 w-8 rounded-full bg-primary flex items-center justify-center text-primary-foreground font-bold">S</div>
             <span className="font-bold text-lg">
               Shubham
             </span>
